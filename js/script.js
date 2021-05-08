23aa1f473a62410a080dd691bc890b131210299e
JuliaTiunova <juliatunn@gmail.com>
2021-05-08 17:58:02 +0300

    add a working mobile-menu, adapt to sm size screen

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const burger = document.querySelector(".burger");
 burger.addEventListener("click", () => {
   document.body.classList.toggle("menu-open");
 });
+
+const mobile = document.querySelector(".mobile");
+mobile.addEventListener("click", () => {
+  document.body.classList.toggle("menu-open");
+});
